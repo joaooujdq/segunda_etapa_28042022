68a5ebbc7b305f7aa0d3d4e071e78c8dd5de2b11
João Marcelo <49964202+joaooujdq@users.noreply.github.com>
2022-04-30 19:05:05 -0300

    codigo finalizado

--- src/segunda_etapa_28042022/Desenha.java
+++ src/segunda_etapa_28042022/Desenha.java
@@ -5,21 +5,24 @@ import java.awt.*;
 import java.util.Arrays;
 
 
+
 public class Desenha extends JFrame {
 
     private double[] soma= new double[10];
 
     private double menor = 100.0;
 
-    private int menorIndex;
+    private String ci1, ci2 ;
+
+    private int menorIndex = -1;
+
+    private int x1 = -1, x2 = -1, y1 = -1, y2 =-1;
 
     private double distanciaTotal=0;
 
     private String caminho = "";
 
     private int encontrado = -1;
-
-    private String repetidos="";
 
     private int[] pontoEscolhido1 = {0, 0}, pontoEscolhido2 = {0, 0};
 
@@ -37,15 +40,16 @@ public class Desenha extends JFrame {
     
 
     public Desenha(String cidade1, String cidade2) {
+
+
         this.setTitle("Segunda Etapa Teste");
-        this.setSize(300, 300);
+        this.setSize(900, 900);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        selecionandoPontos(cidade1, cidade2);
-        resultados[0]=pontoInicial;
-        Caminhos(pontoEscolhido1, pontoEscolhido2, cidade1, cidade2);
-        resultados[resultados.length -1] = pontoFinal;
+        ci1 = cidade1;
+        ci2 = cidade2;
+
     }
 
 
@@ -54,167 +58,284 @@ public class Desenha extends JFrame {
     @Override
     public void paint(Graphics g) {
 
-        g.setColor(Color.red);
+        selecionandoPontos(ci1, ci2);
+        resultados[0]=pontoInicial;
+        Caminhos(pontoEscolhido1, pontoEscolhido2, ci1, ci2);
+        g.setColor(Color.blue);
         DesenhaLinhas(g);
+
     }
 
     public void DesenhaLinhas(Graphics g) {
-        System.out.println(menor);
-
-        System.out.println(caminho);
+        for(int m =0 ; m<10 ; m++)
 
         for(int i=0; i< resultados.length; i++){
             if(resultados[i] != -1){
-                switch (resultados[i]){
-                    case 0:
-                        putPixel(g, pontoA[0], pontoA[1]);
-                        break;
-                    case 1:
-                        putPixel(g, pontoB[0], pontoB[1]);
-                        break;
-                    case 2:
-                        putPixel(g, pontoC[0], pontoC[1]);
-                        break;
-                    case 3:
-                        putPixel(g, pontoD[0], pontoD[1]);
-                        break;
-                    case 4:
-                        putPixel(g, pontoE[0], pontoE[1]);
-                        break;
-                    case 5:
-                        putPixel(g, pontoF[0], pontoF[1]);
-                        break;
-                    case 6:
-                        putPixel(g, pontoG[0], pontoG[1]);
-                        break;
-                    case 7:
-                        putPixel(g, pontoH[0], pontoH[1]);
-                        break;
-                    case 8:
-                        putPixel(g, pontoI[0], pontoI[1]);
-                        break;
-                    case 9:
-                        putPixel(g, pontoJ[0], pontoJ[1]);
-                        break;
+                if(resultados[i] == 0) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoA[0] * 40;
+                        y1 = pontoA[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoA[0] * 40;
+                        y2 = pontoA[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                    }
                 }
-            }
+                if(resultados[i] == 1) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoB[0] * 40;
+                        y1 = pontoB[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoB[0] * 40;
+                        y2 = pontoB[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 2) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoC[0] * 40;
+                        y1 = pontoC[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoC[0] * 40;
+                        y2 = pontoC[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 3) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoD[0] * 40;
+                        y1 = pontoD[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoD[0] * 40;
+                        y2 = pontoD[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 4) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoE[0] * 40;
+                        y1 = pontoE[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoE[0] * 40;
+                        y2 = pontoE[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 5) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoF[0] * 40;
+                        y1 = pontoF[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoF[0] * 40;
+                        y2 = pontoF[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 6) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoG[0] * 40;
+                        y1 = pontoG[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoG[0] * 40;
+                        y2 = pontoG[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 7) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoH[0] * 40;
+                        y1 = pontoH[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoH[0] * 40;
+                        y2 = pontoH[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 8) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoI[0] * 40;
+                        y1 = pontoI[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoI[0] * 40;
+                        y2 = pontoI[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
+                if(resultados[i] == 9) {
+                    if(i == 0 || i%2 == 0) {
+                        x1 = pontoJ[0] * 40;
+                        y1 = pontoJ[1] * 40;
+                        if(i>1){
+                            calculoLinha(g,x1,y1,x2,y2);
+                        }
+                    }else{
+                        x2 = pontoJ[0] * 40;
+                        y2 = pontoJ[1] * 40;
+                        calculoLinha(g, x2,y2,x1,y1);
+                    }
+                }
 
         }
-
+        System.out.println("Distância Percorrida: " + distanciaTotal);
     }
 
     public void Caminhos(int[] p1, int[] p2, String c1, String c2) {
         for (int x = 0; x < 10 && encontrado < 0; x++) {
-                if(vizinhoA.indexOf(c1) != -1 ){
-                    soma[0] =  distancia(p1[0] * 1.0, p1[1] * 1.0, pontoA[0] * 1.0, pontoA[1] * 1.0);
-                }
-                if(vizinhoB.indexOf(c1) != -1){
-                    soma[1] =  distancia(p1[0] * 1.0, p1[1] * 1.0, pontoB[0] * 1.0, pontoB[1] * 1.0);
-                }
-                if(vizinhoC.indexOf(c1) != -1){
-                    soma[2] =  distancia(p1[0] * 1.0, p1[1] * 1.0, pontoC[0] * 1.0, pontoC[1] * 1.0);
-                }
-                if(vizinhoD.indexOf(c1) != -1){
-                    soma[3] =  distancia(p1[0] * 1.0, p1[1] * 1.0, pontoD[0] * 1.0, pontoD[1] * 1.0);
-                }
-                if(vizinhoE.indexOf(c1) != -1){
-                    soma[4] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoE[0] * 1.0, pontoE[1] * 1.0);
-                }
-                if(vizinhoF.indexOf(c1) != -1){
-                    soma[5] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoF[0] * 1.0, pontoF[1] * 1.0);
-                }
-                if(vizinhoG.indexOf(c1) != -1){
-                    soma[6] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoG[0] * 1.0, pontoG[1] * 1.0);
-                }
-                if(vizinhoH.indexOf(c1) != -1){
-                    soma[7] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoH[0] * 1.0, pontoH[1] * 1.0);
-                }
-                if(vizinhoI.indexOf(c1) != -1){
-                    soma[8] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoI[0] * 1.0, pontoI[1] * 1.0);
-                }
-                if(vizinhoJ.indexOf(c1) != -1){
-                    soma[9] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoJ[0] * 1.0, pontoJ[1] * 1.0);
-                }
+            if (vizinhoA.indexOf(c1) != -1 && !repetidos(resultados, 0)) {
+                soma[0] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoA[0] * 1.0, pontoA[1] * 1.0);
+            }
+            if (vizinhoB.indexOf(c1) != -1 && !repetidos(resultados, 1)) {
+                soma[1] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoB[0] * 1.0, pontoB[1] * 1.0);
+            }
+            if (vizinhoC.indexOf(c1) != -1 && !repetidos(resultados, 2)) {
+                soma[2] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoC[0] * 1.0, pontoC[1] * 1.0);
+            }
+            if (vizinhoD.indexOf(c1) != -1 && !repetidos(resultados, 3)) {
+                soma[3] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoD[0] * 1.0, pontoD[1] * 1.0);
+            }
+            if (vizinhoE.indexOf(c1) != -1 && !repetidos(resultados, 4)) {
+                soma[4] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoE[0] * 1.0, pontoE[1] * 1.0);
+            }
+            if (vizinhoF.indexOf(c1) != -1 && !repetidos(resultados, 5)) {
+                soma[5] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoF[0] * 1.0, pontoF[1] * 1.0);
+            }
+            if (vizinhoG.indexOf(c1) != -1 && !repetidos(resultados, 6)) {
+                soma[6] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoG[0] * 1.0, pontoG[1] * 1.0);
+            }
+            if (vizinhoH.indexOf(c1) != -1 && !repetidos(resultados, 7)) {
+                soma[7] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoH[0] * 1.0, pontoH[1] * 1.0);
+            }
+            if (vizinhoI.indexOf(c1) != -1 && !repetidos(resultados, 8)) {
+                soma[8] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoI[0] * 1.0, pontoI[1] * 1.0);
+            }
+            if (vizinhoJ.indexOf(c1) != -1 && !repetidos(resultados, 9)) {
+                soma[9] = distancia(p1[0] * 1.0, p1[1] * 1.0, pontoJ[0] * 1.0, pontoJ[1] * 1.0);
+            }
+            menor = 100;
 
-
-            
-            for(int a =0; a<10;a++){
-                if(soma[a] < menor && soma[a] > 0){
+            for (int a = 0; a < 10; a++) {
+                if (soma[a] < menor && soma[a] > 0) {
                     menor = soma[a];
                     menorIndex = a;
                 }
             }
-            resultados[x] = menorIndex;
-            distanciaTotal = distanciaTotal + menor;
-            System.out.println(menorIndex);
-
-
-            if(menorIndex == 0 ){
-                c1="a";
-                repetidos = "a" + repetidos;
-            }
-            if(menorIndex == 1 ){
-                c1="b";
-                repetidos = "b" + repetidos;
-            }
-            if(menorIndex == 2 ){
-                c1="c";
-                repetidos = "c" + repetidos;
-            }
-            if(menorIndex == 3 ){
-                c1="d";
-                repetidos = "d" + repetidos;
-            }
-            if(menorIndex == 4 ){
-                c1="e";
-                repetidos = "e" + repetidos;
-            }
-            if(menorIndex == 5 ){
-                c1="e";
-                repetidos = "e" + repetidos;
-            }
-            if(menorIndex == 6 ){
-                c1="f";
-                repetidos = "f" + repetidos;
-            }
-            if(menorIndex == 7 ){
-                c1="g";
-                repetidos = "g" + repetidos;
-            }
-            if(menorIndex == 8 ){
-                c1="h";
-                repetidos = "h" + repetidos;
-            }
-            if(menorIndex == 9 ){
-                c1="i";
-                repetidos = "i" + repetidos;
-            }
-            if(menorIndex == 10 ){
-                c1="j";
-                repetidos = "j" + repetidos;
-            }
-
-
-            for(int j =0 ; j<10 ; j++){
+            for (int j = 0; j < 10; j++) {
                 soma[j] = 0;
             }
-            System.out.println(resultados[0]);
-            System.out.println(resultados[1]);
-            System.out.println(resultados[2]);
-            System.out.println(resultados[3]);
-            System.out.println(resultados[4]);
-            System.out.println(resultados[5]);
-            System.out.println(resultados[6]);
-            System.out.println(resultados[7]);
-            System.out.println(resultados[8]);
-            System.out.println(resultados[9]);
-            if(menorIndex == pontoFinal){
-                encontrado++;
-            }
+
+            if (menorIndex != -1) {
+                resultados[x+1] = menorIndex;
+                distanciaTotal = distanciaTotal + menor;
+                switch (menorIndex) {
+                    case 0:
+                        c1 = "a";
+                        break;
+                    case 1:
+                        c1 = "b";
+                        break;
+                    case 2:
+                        c1 = "c";
+                        break;
+                    case 3:
+                        c1 = "d";
+                        break;
+                    case 4:
+                        c1 = "e";
+                        break;
+                    case 5:
+                        c1 = "f";
+                        break;
+                    case 6:
+                        c1 = "g";
+                        break;
+                    case 7:
+                        c1 = "h";
+                        break;
+                    case 8:
+                        c1 = "i";
+                        break;
+                    case 9:
+                        c1 = "j";
+                        break;
+                }
+                if (menorIndex == pontoFinal) {
+                    encontrado++;
+                }
+
+                menorIndex = -1;
             }
         }
+        }
 
+    public boolean repetidos(int[] resultados, int valor)
+    {
+        boolean teste = false;
+        for (int element : resultados) {
+            if (element == valor) {
+                teste = true;
+                break;
+            }
+        }
+    return teste;
+    }
 
+    public void calculoLinha(Graphics g, int xi, int yi, int xf, int yf) {
+        int x = xi, y = yi, d=0, dx = xf-xi, dy = yf-yi, c, m, incX=1, incY=1;
+
+        if(dx < 0) {incX = -1; dx = -dx;}
+        if(dy < 0) {incY = -1; dy = -dy;}
+
+        if(dy <= dx) {
+            c = 2 * dx; m = 2 * dy;
+            if(incX < 0) dx++;
+            for(;;) {
+                putPixel(g,x,y);
+                if (x == xf) break;
+                x += incX;
+                d += m;
+                if(d >= dx) {y += incY; d -= c;}
+            }
+        } else {
+            c = 2 * dy; m = 2 * dx;
+            if(incY < 0) dy++;
+            for(;;) {
+                putPixel(g,x,y);
+                if (y == yf) break;
+                y += incY;
+                d += m;
+                if(d >= dy) {x += incX; d -= c;}
+            }
+
+        }
+    }
 
         public double distancia( double x1, double y1, double x2, double y2){
 
--- src/segunda_etapa_28042022/Janela.java
+++ src/segunda_etapa_28042022/Janela.java
@@ -17,16 +17,10 @@ public class Janela {
          private int encontrouCidade2 = -1;
 
         public Janela() {
-            System.out.println(todasCidades.length);
+
             cidade1 = JOptionPane.showInputDialog("Digite a letra correspondete a primeira cidade");
             cidade2 = JOptionPane.showInputDialog("Digite a letra correspondete a segunda cidade");
             for(int x = 0;  x < todasCidades.length; x++) {
-
-
-                System.out.println(cidade1);
-                System.out.println(cidade2);
-                System.out.println(todasCidades.getClass().getSimpleName());
-
 
                if (cidade1.indexOf(todasCidades[x]) != -1 && !cidade1.equals(cidade2)  && encontrouCidade1 != 0){
                    entradaInvalida++;
@@ -40,7 +34,7 @@ public class Janela {
                    encontrouCidade2++;
                }
             }
-            System.out.println(entradaInvalida);
+
             if( entradaInvalida < 1) {
                 JOptionPane.showMessageDialog(null,"Entrada inválida. Tente novamente.");
             }else if(entradaInvalida> 0 ){
